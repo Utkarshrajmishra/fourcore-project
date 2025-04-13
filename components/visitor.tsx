788957922ab8e61e10246c6d66e660b4fa1b1643
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-04-13 16:49:48 +0530

    Add: Visitor

--- components/visitor.tsx
+++ components/visitor.tsx
@@ -82,7 +82,7 @@ export function Visitors() {
   return (
     <section className="h-fit w-full border border-gray-200 rounded-xl p-4 bg-white">
       <p className="text-sm font-medium text-blue-950">Visitors Insights</p>
-      <ChartContainer config={chartConfig} className="h-36 mt-4 w-full">
+      <ChartContainer config={chartConfig} className="h-39 mt-4 w-full">
         <ResponsiveContainer width="100%" height="100%">
           <LineChart
             data={customerData}
